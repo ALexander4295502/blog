e8cd1b41255b620a10fd1de6e7cd5e5d4eb03310
Zheng Yuan <yuanzhengstl@gmail.com>
2019-11-16 15:51:02 -0800

    minor update

---
+++ example/js-scope.js
@@ -0,0 +1,10 @@
+var a = 1;
+(function b() {
+	a = 10;
+    return;
+    // if you comment out this line, scope bubble up won't trigger 
+    // since it declare a local varialbe in the function-level scope
+    // and hoist it before operations in this scope
+	function a() {}
+})()
+console.log(a);
